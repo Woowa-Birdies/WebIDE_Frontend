23992717fd22e26f8ddddc5c0ef826447ee7d76a
Jhsysng <ysng1118@naver.com>
2024-02-23 15:58:49 +0900

    feat: axios연결

--- src/components/chat/Messages.js
+++ src/components/chat/Messages.js
@@ -9,14 +9,13 @@ export default function Messages({ projectId = 1 }) {
   const [loading, setLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
   const listContainerRef = useRef(null);
-
+  const url = 'http://localhost:8080';
   useEffect(() => {
     const fetchRoomInfo = async () => {
       try {
-        //const response = await axios.get(`http://localhost:8080/chat/room/${projectId}`);
-        //setRoomId(response.data.roomId);
-        //todo : roomId가 없어서 임시로 1로 설정
-        const response = {
+        const response = await axios.get(`${url}/chat/room/${projectId}`);
+        setRoomInfo(response.data);
+       /*const response = {
             projectId: 1,
             roomId: 1,
             admin: {
@@ -29,8 +28,7 @@ export default function Messages({ projectId = 1 }) {
               profileImage: "http://aws.s3/xx.png",
               nickname: "구름",
             },
-          };
-        setRoomInfo(response);
+          };*/
       } catch (error) {
         console.error('방 정보를 불러오는데 실패했습니다.', error);
       }
@@ -47,75 +45,39 @@ export default function Messages({ projectId = 1 }) {
   }, [roomInfo]); 
 
   const fetchChats = async (page) => {
-    /*
-        if (loading || !hasMore) return;
-
+    
+    if (loading || !hasMore) return;
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:8080/chat/fetch', {
+      const response = await axios.post('http://localhost:8080/chat', {
         roomId: roomId,
         messageTimeStamp: new Date().toISOString(), // 현재 시점의 ISO 타임스탬프, 실제 사용 시 조정 필요
         page: page,
-        size: 3
+        size: 100
       });
 
       // 응답으로 받은 데이터를 처리
       if (response.data && response.data.content) {
+        response.data.content = response.data.content.map(message => {
+          const isAdmin = message.sender === roomInfo.admin.userId;
+          const senderNickname = isAdmin ? roomInfo.admin.nickname : roomInfo.user.nickname;
+          const profileImage = isAdmin ? roomInfo.admin.profileImage : roomInfo.user.profileImage;
+
+          return {
+            ...message,
+            isAdmin,
+            senderNickname,
+            profileImage
+          };
+        });
         setChats(prevChats => [...prevChats, ...response.data.content]);
         setPage(prevPage => prevPage + 1);
-        setHasMore(response.data.content.length > 0); 
+        setHasMore(response.data.content.length > 0);
       }
 
       setLoading(false);
     } catch (error) {
       console.error('채팅 데이터를 불러오는 중 에러가 발생했습니다.', error);
-      setLoading(false);
-
-    }*/
-    if (!loading && hasMore && roomInfo.admin && roomInfo.user) {
-      setLoading(true);
-      // 더미 데이터 반환 시뮬레이션
-      const dummyData = {
-        content: [
-          {
-            messageId: "5f8d04b3eaf7681a30c7f562",
-            roomId: 1,
-            message: "안녕하세요, 첫 번째 메시지입니다.",
-            sender: 1212,
-            readUser: { user1: true, user2: false },
-            createdAt: "2023-03-01T12:00:00"
-          },
-          {
-            messageId: "5f8d04b4eaf7681a30c7f563",
-            roomId: 1,
-            message: "이것은 두 번째 메시지입니다.",
-            sender: 1,
-            readUser: { user1: true, user2: false },
-            createdAt: "2023-03-01T12:10:00"
-          }
-        ],
-        last: true
-      };
-      console.log(roomInfo);
-      console.log('dummyData : ', dummyData.content)
-      dummyData.content = dummyData.content.map(message => {
-        const isAdmin = message.sender === roomInfo.admin.userId;
-        const senderNickname = isAdmin ? roomInfo.admin.nickname : roomInfo.user.nickname;
-        const profileImage = isAdmin ? roomInfo.admin.profileImage : roomInfo.user.profileImage;
-  
-        return {
-          ...message,
-          isAdmin,
-          senderNickname,
-          profileImage
-        };
-      });
-      console.log('dummyData : ', dummyData.content)
-
-      // 더미 데이터를 상태에 설정
-      setChats((prevChats) => [...prevChats, ...dummyData.content]);
-      //setChats((prevChats) => [...prevChats, ...response.data.content])
-      setHasMore(!dummyData.last);
       setLoading(false);
     }
   };
@@ -142,27 +104,11 @@ export default function Messages({ projectId = 1 }) {
     }
   }, [chats]);
 
-  /*
-  return (
-    <div ref={listContainerRef} className="overflow-auto h-full" style={{height: '500px'}}>
-      <List 
-        dataSource={chats}
-        renderItem={(item) => (
-          <List.Item key={item.messageId} style={{ borderBottom: 'none' }}>
-            <Skeleton avatar title={false} loading={item.loading} active>
-              <Message message={item} />
-            </Skeleton>
-          </List.Item>
-        )}
-      />
-    </div>
-  );*/
   function addDateDividers(messages) {
     const enhancedMessages = [];
     let lastDate = null;
   
     messages.forEach((message) => {
-      // createdAt 속성의 존재 여부를 확인
       console.log('message : ', message);
       if (message.createdAt) {
         const messageDate = message.createdAt.split('T')[0];
@@ -180,7 +126,7 @@ export default function Messages({ projectId = 1 }) {
   return (
     <div ref={listContainerRef} className="overflow-auto h-full" style={{ height: '500px' }}>
       <List
-        dataSource={addDateDividers(chats)} // 날짜 구분자가 추가된 메시지 목록
+        dataSource={addDateDividers(chats)}
         renderItem={item => {
           // 날짜 구분자 렌더링
           if (item.type === 'dateDivider') {
